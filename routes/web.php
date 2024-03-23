fb1d6d800c8a62858a919fa203666f936cf652d7
JeremiahSalim <jeremiahsalim12@gmail.com>
2024-03-23 16:01:28 +0700

    testing aja

--- routes/web.php
+++ routes/web.php
@@ -22,28 +22,16 @@ Route::get('hello',function(){
     return view('student');
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-Route::get('home',function(){
-=======
-
-
-//hai
-
 Route::get('/home', function() {
     return view('home');
 });
 
-//hai
-
-
-
->>>>>>> 4df2be71fa02970f6b2ec4e23062ef06a1e85303
-=======
-//ini komen
 Route::get('home', function() {
->>>>>>> 222abc1f5c73ee953ba2f28237870eb417410369
     return 'home';
 });
 
->>>>>>> 222abc1f5c73ee953ba2f28237870eb417410369
+Route::get('hahaha', function() {
+    return 'hahaha';
+});
+
+
