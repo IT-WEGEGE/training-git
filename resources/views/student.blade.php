90aeffd179e9a93b50fbc2293fdbf25d4cff2b39
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-03-23 10:26:41 +0700

    fix: handle cors OFF

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -40,7 +40,7 @@
                 <label class="text-gray-800 font-bold text-lg pl-5 m-0">Address</label>
                 <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required type="text" id="address" placeholder="Address">
             </div>
-            <button type="button" onclick="addStudent()" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
+            <button type="button" onclick="addStudent()" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Add</button>
         </div>
     </body>
     <script>
